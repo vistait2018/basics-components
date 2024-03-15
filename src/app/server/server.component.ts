abcefd961bf04489d8eb0d31b71101f2d7898b60
Java20091@ <jidedorcas@gmail.com>
2024-03-15 12:21:02 +0100

    componentsanddirectives

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -6,4 +6,29 @@ import { Component } from '@angular/core';
 })
 export class ServerComponent {
 
+    serverId:number =10;
+    serverName:String = 'Telnet Server';
+    serverStatus:boolean = false;
+
+    constructor(){
+
+      this.swapStatus();
+      
+    }
+
+    swapStatus(){
+      setInterval( () => {
+        console.log(this.serverStatus);
+        this.serverStatus = !this.serverStatus;
+        return this.serverStatus;
+      }, 10000 );
+      
+    }
+    
+    // showTextInput(event:Event){
+    // console.log(event);
+    // this.serverName = (<HTMLInputElement>event.target).value;
+    // }
+    
+
 }
